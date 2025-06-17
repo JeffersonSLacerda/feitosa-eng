2fbb19271feaa8c6db335f6828a9719157e7daf0
Jefferson Lacerda <47826649+JeffersonSLacerda@users.noreply.github.com>
2025-06-17 13:34:42 -0300

    chore: Add authentication schema and integrate better-auth for user management

---
+++ src/lib/auth.ts
@@ -0,0 +1,25 @@
+import { db } from "@src/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+ 
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    usePlural: true,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    modelName: "usersTable",
+  },
+  session: {
+    modelName: "sessionsTable",
+  },
+  account: {
+    modelName: "accountsTable",
+  },
+  verification: {
+    modelName: "verificationsTable",
+  },
+})
